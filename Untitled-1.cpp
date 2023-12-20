1148a637b032d0ea6734b6e585a49f9ff996c00d
oceannoc <154425585+oceannoc@users.noreply.github.com>
2023-12-20 19:54:32 +0700

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+class abc
+{    public:
+    int x,y;
+   
+    void   getdis()
+    {
+        cout<< x+y;
+
+    }
+
+
+
+};
+
+class xyz : abc
+{   public:
+    int z;
+    
+     void getdis( int k)
+    {
+
+        cout << x+y+z +k;
+
+    }
+
+
+
+};
+
+
+
+
+int main()
+{
+   xyz a;
+   a.x= 3;
+   a.y= 4;
+   a.z= 6;
+   a.getdis(2);
+    
+}
+
+
+
